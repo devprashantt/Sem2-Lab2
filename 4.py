fd02ff44f0f3f2d73b689882460bff0c2b8e090b
Prashant Kumar Singh <pk3076889@gmail.com>
2022-05-11 14:48:44 +0530

    Lab2

--- 4.py
+++ 4.py
@@ -1,4 +1,4 @@
-number = int(input("What is the number"))
+number = int(input("What is the number-\n"))
 
 sum = 0
 
@@ -6,5 +6,6 @@ if number<=15 and number>=9:
     for i in range(1,15):
         if number%i == 0:
             sum = sum + i
+    print(sum)        
 else:
-    print("Number out of range (9 to 15")
+    print("Number out of range (9 to 15)")
